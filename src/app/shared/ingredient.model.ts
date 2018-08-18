12ce666c0fc5d9fe4088847f61709b31c85e2b28
sumit <sumit1523@gmail.com>
2018-08-19 01:03:12 +0530

    18th Aug commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    // public name : string;
+    // public amount :number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // } alternate way
+    constructor(public name: string, public amount: number){
+    }
+}
